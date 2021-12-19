37fe0da746076e863f977b97303f14ae5be689d4
eilla1 <git@ella.cx>
2021-12-19 02:17:08 -0800

    no thoughts head empyt

---
+++ data/map/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="tileset" tilewidth="32" tileheight="32" spacing="2" tilecount="9" columns="3">
+ <image source="../img/tileset.png" width="100" height="100"/>
+</tileset>
